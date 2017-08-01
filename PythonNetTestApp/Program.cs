7826b7de6e1331e58150f7b6d07c154d7f0903d5
Will Robinson <wrobinson@genscape.com>
2017-08-01 11:30:07 -0400

    Initial commit

---
+++ PythonNetTestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Python.Runtime;
+
+namespace PythonNetTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting python");
+            //PythonEngine.PythonHome = "C:/FakeDir";
+            PythonEngine.Initialize();
+            Console.WriteLine("Python started");
+            Console.ReadLine();
+        }
+    }
+}
